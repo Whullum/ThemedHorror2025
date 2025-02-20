a949d7314a3b01e718952a8e8553b4a4c449b981
tamarub <123705249+tamarub@users.noreply.github.com>
2025-02-20 11:39:05 -0500

    updated intro script as in main branch
    fixing some conflicts here

--- Assets/Art/UI/UI Scripts/dialogueIntro.cs
+++ Assets/Art/UI/UI Scripts/dialogueIntro.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Runtime.InteropServices;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class dialogueIntro : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
     public string[] lines;
     public float textSpeed;
+    public string mainSceneName;
     private int index;
-    public GameObject arrow;
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        arrow.SetActive(false);
         textComponent.text=string.Empty;
         StartDialogue();
     }
@@ -55,12 +56,10 @@ public class dialogueIntro : MonoBehaviour
             yield return new WaitForSeconds(textSpeed);
 
         }
-        arrow.SetActive(true);
     }
 
     void NextLine()
     {
-        arrow.SetActive(false);
         if (index<lines.Length-1)
         {
             index++;
@@ -70,6 +69,7 @@ public class dialogueIntro : MonoBehaviour
         else
         {
             gameObject.SetActive(false);
+            SceneManager.LoadScene(mainSceneName);
         }
     }
 }
